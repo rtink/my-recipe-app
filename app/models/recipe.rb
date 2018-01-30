ba9a886c96bde35b3b165655651cbae09bdd7a28
Rhonda Tinkham <rtinkham@live.com>
2018-01-30 22:59:45 +0000

    Build chef and recipe relationship

---
+++ app/models/recipe.rb
@@ -0,0 +1,9 @@
+class Recipe < ApplicationRecord
+  
+  belongs_to :chef
+  
+  validates :name, presence: true
+  validates :description, presence: true, length: { minimum: 5, maximum: 500 }
+  validates :chef_id, presence: true
+  
+end
---
+++ test/models/recipe_test.rb
@@ -0,0 +1,33 @@
+require "test_helper"
+
+class RecipeTest < ActiveSupport::TestCase
+
+  def setup
+    @chef = Chef.create!(chefname: "patty", email: "patty@example.com")
+    @recipe = @chef.recipes.build(name: "vegetable", description: "great vegetable recipe")
+  end
+  
+  test "recipe should be valid" do
+    assert @recipe.valid?
+  end
+  
+  test "description should be present" do
+    @recipe.description = " "
+    assert_not @recipe.valid?
+  end
+  
+  test "description shouldn't be less than 4 chars" do
+    @recipe.description = "a" * 3
+    assert_not @recipe.valid?
+  end
+  
+  test "description shouldn't be more than 500 chars" do
+    @recipe.description = "a" * 501
+    assert_not @recipe.valid?
+  end
+  
+  test "recipe without chef should be invalid" do
+    @recipe.chef_id = nil
+    assert_not @recipe.valid?
+  end
+end
